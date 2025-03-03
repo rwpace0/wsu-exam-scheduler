bd4f026010640ca4c20806b5a5ad1f31254ebc52
Reid Pace <rwpace06@gmail.com>
2025-03-02 21:11:36 -0800

    welcome page start

---
+++ react/src/pages/Welcome.jsx
@@ -0,0 +1,10 @@
+function Welcome() {
+    return (
+        <div className="bg-customGray h-screen w-screen flex items-center justify-center">
+            <h1 className="font-primary ">Welcome to the React App</h1>
+            
+        </div>
+    );
+}
+
+export default Welcome;
